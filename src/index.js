e9b8dbdc20b86d98ea030184ca94fd9e8d9b6636
Ricardo <ricardo270298@gmail.com>
2022-07-30 17:13:01 -0500

    bug fix with env Script

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Template from '@templates/Template.js';
 import '@styles/main.css';
 import '@styles/var.styl';
-console.log('hola')
 
 (async function App() {
   const main = null || document.getElementById('main');
